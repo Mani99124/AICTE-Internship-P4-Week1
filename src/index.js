b7530ab03fdab88dccf5984210a5b8c3e0582262
Manikanth Etikyala <manikanthetikyala174@gmail.com>
2025-02-24 17:40:49 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
